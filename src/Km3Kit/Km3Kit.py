c3403bf43fcdcc437f97787e233bcf65d5ca3a88
Amid <amid.nayerhoda@gmail.com>
2024-11-11 23:24:12 +0100

    Update Km3Kit.py

--- src/Km3Kit/Km3Kit.py
+++ src/Km3Kit/Km3Kit.py
@@ -1,1 +1,14 @@
 """Main module."""
+from IO.io import Read
+
+
+DATA, NEUTRINO, MUON = Read("arca21_bdt", verbose=True)
+# Example: Access data from "branch1"
+branch1_data = DATA["E"]
+
+# Create a histogram of branch1 data (for example)
+#plt.hist(branch1_data, bins=50)
+#plt.xlabel("Branch1 Values")
+#plt.ylabel("Frequency")
+#plt.title("Histogram of Branch1")
+#plt.show()
