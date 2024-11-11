c3a004c62833ec1970826d9fac2bedc77fb6fed5
Amid <amid.nayerhoda@gmail.com>
2024-11-11 21:57:43 +0100

    Update rootio.py

--- src/km3kit/IO/rootio.py
+++ src/km3kit/IO/rootio.py
@@ -1,9 +1,24 @@
 import uproot
-from yml_Loader import Loader
+
 
 def readroot(file_list):
     file_content = {}
-    file_content["headerTree"] = uproot.concatenate([f"{file}:headerTree" for file in file_list])
-    file_content["E"]          =  uproot.concatenate([f"{file}:E" for file in file_list])
-    file_content["T"]          =  uproot.concatenate([f"{file}:T" for file in file_list])
+    try:
+        file_content["headerTree"] = uproot.concatenate([f"{file}:headerTree" for file in file_list])
+    except Exception as e:
+        print(f"Error reading 'headerTree': {e}")
+        file_content["headerTree"] = None
+
+    try:
+        file_content["E"] = uproot.concatenate([f"{file}:E" for file in file_list])
+    except Exception as e:
+        print(f"Error reading 'E': {e}")
+        file_content["E"] = None
+
+    try:
+        file_content["T"] = uproot.concatenate([f"{file}:T" for file in file_list])
+    except Exception as e:
+        print(f"Error reading 'T': {e}")
+        file_content["T"] = None
+
     return file_content
