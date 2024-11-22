7368a0991c2b54ba2437328d9bb8605fd2ed717c
Amid <amid.nayerhoda@gmail.com>
2024-11-22 18:08:24 +0100

    -

---
+++ src/Km3Kit/IO/__init__.py
@@ -0,0 +1,7 @@
+# src/Km3Kit/IO/__init__.py
+
+# Expose key modules or functions for ease of import
+from .io import DF_
+from .rootio import load_dst
+
+__all__ = ["DF_", "load_dst"]
---
+++ src/Km3Kit/utils/__init__.py
@@ -0,0 +1,12 @@
+# src/Km3Kit/utils/__init__.py
+
+# Expose key utility functions or classes
+from .yaml_utils import Loader, load_branches_config
+from .tools import report_time_interval, report_memory_usage
+
+__all__ = [
+    "Loader",
+    "load_branches_config",
+    "report_time_interval",
+    "report_memory_usage",
+]
