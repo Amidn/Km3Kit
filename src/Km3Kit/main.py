f5c2b81582617fe34ac31800099004a139a2ca4c
Amid <amid.nayerhoda@gmail.com>
2024-11-24 19:06:08 +0100

    Update main.py

--- src/Km3Kit/main.py
+++ src/Km3Kit/main.py
@@ -3,8 +3,9 @@ from utils.yml_utils import readConfigs
 
 # Read the configuration
 configs = readConfigs()  # Should return a dictionary
-print(type(configs))  # Should be <class 'dict'>
-print(configs) 
+configs = readConfigs()
+print("Raw configs:", configs)  # To inspect the parsed data
+print(type(configs))  # To confirm the data type
 # Access the desired value
 saving_dir = configs["FileConfig"]["Saving_Dir"]
 print(f"Saving directory: {saving_dir}")
