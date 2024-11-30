ee9c0d4176cadf51b741dd7cb833b85c23919998
Amid <amid.nayerhoda@gmail.com>
2024-11-30 15:44:30 +0100

    Update __init__.py

--- src/Km3Kit/IO/__init__.py
+++ src/Km3Kit/IO/__init__.py
@@ -3,6 +3,6 @@
 # Expose key modules or functions for ease of import
 from .io import read, process_dfs
 from .rootio import load_dst, pd_dataFrame
-from fitsio import create_fits_file
+from .fitsio import create_fits_file
 
 __all__ = ["read", "process_dfs", "load_dst", "pd_dataFrame", "create_fits_file"]
