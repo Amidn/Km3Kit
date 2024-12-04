e8510540dea1813ff41b82757b37ef794d47ee1e
Amid <amid.nayerhoda@gmail.com>
2024-12-04 13:24:16 +0100

    =

--- src/Km3Kit/main.py
+++ src/Km3Kit/main.py
@@ -8,6 +8,7 @@ from Km3Kit.IO.fitsio import create_fits_file
 
 df_data = pd.read_hdf("/sps/km3net/users/amid/DataSets/Km3KitProductions/arca21_bdt_converted_Data.h5", key="data")
 print(df_data.columns)
+print(df_data.head())
 create_fits_file("arca21_bdt" ,"config/fits_configs.yml", df_data, "/sps/km3net/users/amid/DataSets/Km3KitProductions/")
 print ("Done")
 #f = get_dataset_version("arca21_bdt", "MMAA")
