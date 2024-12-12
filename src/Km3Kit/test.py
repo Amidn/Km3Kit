639900bb44b5f9b9f97e00a69023288fa16038cd
Amid <amid.nayerhoda@gmail.com>
2024-12-12 13:45:50 +0100

    Update test.py

--- src/Km3Kit/test.py
+++ src/Km3Kit/test.py
@@ -2,18 +2,16 @@ from Km3Kit import KM3NetIRFGenerator
 
 
 # Provide the necessary file names and optional weight factor
-filename_nu = '/home/saturn/capn/mpo1217/cta/mcv5.1.bdt/bdt/mcv5.1.km3_numuCC.ALL.dst.bdt.root'
-filename_nubar = '/home/saturn/capn/mpo1217/cta/mcv5.1.bdt/bdt/mcv5.1.km3_anumuCC.ALL.dst.bdt.root'
-filename_mu10 = '/home/saturn/capn/mpo1217/cta/mcv5.1.bdt/bdt/mcv5.2.mupage_10T.sirene_mupage.ALL.bdt.root'  
-filename_mu50 = '/home/saturn/capn/mpo1217/cta/mcv5.1.bdt/bdt/mcv5.2.mupage_50T.sirene_mupage.ALL.bdt.root'
+filename_nu = ' /sps/km3net/users/amid/production/arca21/Coords_Merged/v2/mcv8.1.all.gsg_numu-CCHEDIS_1e2-1e8GeV.sirene.jterbr.jchain.aashower.dst.bdt_casc.bdt_trk.root"
+filename_nubar = ' /sps/km3net/users/amid/production/arca21/Coords_Merged/v2/mcv8.1.all.gsg_anumu-CCHEDIS_1e2-1e8GeV.sirene.jterbr.jchain.aashower.dst.bdt_casc.bdt_trk.root"
+filename_mu = ' /sps/km3net/users/amid/production/arca21/Coords_Merged/v2/mcv8.1.all.mupage_tuned_100G.sirene.jterbr.jchain.aashower.dst.bdt_casc.bdt_trk.root"
 save_dir = "/"
 
 # Create an instance of KM3NetIRFGenerator
 irf_generator = KM3NetIRFGenerator(
     filename_nu=filename_nu,
     filename_nubar=filename_nubar,
-    filename_mu10=filename_mu10,
-    filename_mu50=filename_mu50,
+    filename_mu=filename_mu,
     save_dir = save_dir
 )
 
